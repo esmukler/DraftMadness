670895f90439163d39d6218d6198934e3afce542
Eli Smukler <eli@berecruited.com>
2016-03-06 14:37:22 -0800

    Manually add encrypted_users

--- db/migrate/20160214225757_create_users.rb
+++ db/migrate/20160214225757_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email,    null: false
       t.string :password, null: false
+      t.string :encrypted_password, null: false, default: '', limit: 128
 
       t.timestamps
     end
