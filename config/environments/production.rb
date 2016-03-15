4637ae8c9c39acd0310ad38f8f3ffb43bf3527bc
Eli Smukler <eli@berecruited.com>
2016-03-15 14:40:30 -0700

    Bring back servername

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, nil, {
+  config.cache_store = :dalli_store, (ENV["MEMCACHIER_SERVERS"] || "").split(","), {
     failover: true,
     socket_timeout: 1.5,
     socket_failure_delay: 0.2,
