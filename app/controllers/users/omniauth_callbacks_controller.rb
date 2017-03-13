6764cfad288eb6d822a36a7e163be0d32fdc95cb
Eli Smukler <eli@berecruited.com>
2017-03-13 09:26:21 -0700

    Remember oauths

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
+      remember_me(@user)
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
