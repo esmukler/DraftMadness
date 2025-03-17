07ad46ec6b43d37cac0a0ae019876a884b2bd827
esmukler <eli@smukler.com>
2025-03-16 18:48:44 -0700

    Add 2025 teams; update automate draft job so it can be run in the console

---
+++ app/jobs/automate_draft_job.rb
@@ -0,0 +1,60 @@
+class AutomateDraftJob < ApplicationJob
+  queue_as :default
+
+  def perform(league_id)
+    league = League.find(league_id)
+    owners = league.owners.sort_by(&:draft_pick)
+    owner_idx = 2
+    forward_direction = true
+    schools = get_schools_for(league)
+    fake_cdp = league.current_draft_pick
+
+    ActiveRecord::Base.transaction do
+      while schools.count > 1
+        school = schools.shift
+        owner = owners[owner_idx]
+        create_os(owner, school)
+        owner_idx, forward_direction = update_markers(owner_idx, forward_direction)
+      end
+    end
+  end
+
+  private
+
+  def update_markers(idx, forward_direction)
+    if forward_direction
+      if idx == 7
+        [7, false]
+      else
+        [idx + 1, true]
+      end
+    else
+      if idx == 0
+        [0, true]
+      else
+        [idx - 1, false]
+      end
+    end
+  end
+
+  def fake_create_os(owner, school)
+    puts "[#{owner.league.current_draft_pick}]...#{owner.team_name}-(#{school.full_name_and_seed})"
+  end
+
+  def get_schools_for(league)
+    School.current.includes(:seed).where.not(id: league.school_ids).sort_by do |school|
+      [school.seed_number, rand(4)]
+    end
+  end
+
+  def create_os(owner, school)
+    os = OwnerSchool.create(
+      owner: owner,
+      league: owner.league,
+      school: school,
+      draft_pick: owner.league.current_draft_pick
+    )
+    owner.league.current_draft_pick += 1
+    owner.league.save
+  end
+end
